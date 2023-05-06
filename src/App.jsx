d3a9dde7a044932ca8ffa4a27d1e647f56743c56
Zoa Young Kwon <Kzoyoung@naver.com>
2023-05-06 23:43:31 +0900

    feat: 미세먼지과제 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const res = await fetch("http://openapi.seoul.go.kr:8088/4d435068454b7a6f38354367495348/json/RealtimeCityAir/1/25/");
+  // const res2 = await res.json();
+  // res2.RealtimeCityAir.row
+  
+  const [row,setRow] = useState([]);
+
+  const loadRow =()=>{
+    if(row.length ===0){
+  
+      const res = fetch("http://openapi.seoul.go.kr:8088/4d435068454b7a6f38354367495348/json/RealtimeCityAir/1/25/").then(
+      function(res2){
+        res2.json().then(function(res3){
+          setRow(res3.RealtimeCityAir.row);
+        })
+      }
+    )
+    }
+  }
+
+ 
 
   return (
     <>
@@ -16,16 +36,37 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Resort</h1>
-      <div className="card">
+      <h1>Vite + React</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR hello
-          안녕하세요
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      </div> */}
+
+      <button onClick={loadRow}>로드</button> 
+
+      <table>
+        <thead>
+          <th>이름</th>
+          <th>pm10</th>
+          <th>o3</th>
+          <th>상태</th>
+        </thead>
+        <tbody>
+          {row.map(function(obj){
+            return <tr>
+              <td>{obj.MSRSTE_NM}</td>
+              <td>{obj.PM10}</td>
+              <td>{obj.O3}</td>
+              <td>{obj.IDEX_NM}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
