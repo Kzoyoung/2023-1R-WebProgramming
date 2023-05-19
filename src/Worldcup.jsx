d4cd1c9a5f379f0374886767398c527b3154b2ed
Zoa Young Kwon <Kzoyoung@naver.com>
2023-05-19 23:24:24 +0900

    feat:저장기능 저장된값정렬 progressbar 추가

--- src/Worldcup.jsx
+++ src/Worldcup.jsx
@@ -1,4 +1,4 @@
-import { useEffect,useState } from 'react'
+import { isValidElement, useEffect,useState } from 'react'
 import p1 from './assets/김치찌개.jpg'
 import p2 from './assets/깻잎.jpg'
 import p3 from './assets/꽃게짬뽕.jpg'
@@ -17,6 +17,7 @@ import p15 from './assets/청양고추라면.jpg'
 import p16 from './assets/칼빔면.jpg'
 import versus from './assets/versus.png'
 import './Worldcup.css'
+
 
 function Worldcup(){
     const candidate = [
@@ -37,15 +38,39 @@ function Worldcup(){
         {name:'청양고추라면', src:p15},
         {name:'칼빔면', src:p16},
     ]
-
+    let sum = 0;
     const [game,setGame]= useState([]);
     const [round,setRound]= useState(0);
     const [nextGame,setNextGame]=useState([]);
     const [Lstate, setLState]=useState(0);
     const [Rstate, setRState]=useState(0);
     const [isDisabled,setIsDisabled]=useState(false);
+    const [stat,setStat]=useState({
+        "김치찌개":0,
+        "깻잎":0,
+        "꽃게짬뽕":0,
+        "두부김치":0,
+        "둥지냉면":0,
+        "라면볶이":0,
+        "리얼치즈":0,
+        "마라탕면":0,
+        "쌀국수":0,
+        "앵그리":0,
+        "야끼우동":0,
+        "와사비":0,
+        "진짜진짜":0,
+        "진짬뽕":0,
+        "청양고추라면":0,
+        "칼빔면":0,
+    });
 
     useEffect(()=>{
+
+        const 문자열 = localStorage.getItem("월드컵");
+        if(문자열 != null){
+            setStat(JSON.parse(문자열));
+        }
+
         setGame(candidate.map(c=>{
             return {name: c.name, src: c.src, order: Math.random()}
         }).sort((l,r)=>{
@@ -63,54 +88,97 @@ function Worldcup(){
     },[round]);
 
     if(game.length===1){
+        localStorage.setItem("월드컵",JSON.stringify(stat));
+        Object.keys(stat).map((name)=>{
+            sum += stat[name];
+            console.log({sum});
+        })
         return (
         <div id="finish">
             <p className='header'>이상형 월드컵 우승</p>
-            <img src={game[0].src}/><p id="win">{game[0].name}</p>
+            <img src={game[0].src} id="winImg"/><p id="win">{game[0].name}  {stat[game[0].name]} 번승리</p>
+            
+                
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th>이름</th>
+                        <th>우승횟수</th>
+                        <th id="bar"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Object.keys(stat).sort((l,r)=>{
+                            return stat[r] - stat[l];
+                        }).map(name=>{
+                            return (
+                                <tr key={name}>
+                                    <td>{name}</td>
+                                    <td>{stat[name]}</td>
+                                    <td>
+                                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow={stat[name]} aria-valuemin="0" aria-valuemax="100">
+                                        <div className="progress-bar" style={{width : stat[name]/sum*100 +"%" }}></div>
+                                    </div>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                    
+                </tbody>
+            </table>
         </div>
         )
     }
-    
+
+    const left = round*2;
+    const right = round*2+1;
+    console.log(stat);
     if(game.length===0 || round+1 > game.length/2) return<p>로딩중입니다</p>
 
+    const leftFunction = ()=>{
+        
+        if(!isDisabled){
+            setLState(1);
+            setIsDisabled(true);
+            setTimeout(()=>{
+                setStat({...stat,[game[left].name]:stat[game[left].name]+1});
+                setNextGame((prev)=>prev.concat(game[left]))
+                setRound(round=>round+1);
+                setLState(0);
+                setIsDisabled(false);
+            },1000);
+        }
+    }
+
+    const rightFunction = ()=>{
+        
+        if(!isDisabled){
+            setRState(1);
+            setIsDisabled(true);
+            setTimeout(()=>{
+                setStat({...stat,[game[right].name]:stat[game[right].name]+1});
+                setNextGame((prev)=>prev.concat(game[right])) 
+                setRound(round=>round+1);
+                setRState(0);
+                setIsDisabled(false);
+            },1000);
+        }
+    }
     return (<div className='mainbox'>
         <p className='header'>라면 월드컵{round+1}/{game.length/2} <b>{game.length===2?"결승":game.length+"강"}</b></p>
         <img id="versus" src={versus}/>
         
         <div id={"limg"+Lstate}>
-            <p id="nameL">{game[round*2].name}</p>
-            <img id="img" src={game[round*2].src} onClick={()=>{
-                if(!isDisabled){
-                    setLState(1);
-                    setIsDisabled(true);
-                    setTimeout(()=>{
-                        setNextGame((prev)=>prev.concat(game[round*2]))
-                        setRound(round=>round+1);
-                        setLState(0);
-                        setIsDisabled(false);
-                    },1000);
-                }
-                   
-                    
-            }}/>
+            <p id="nameL">{game[left ].name}</p>
+            <img id="img" src={game[left ].src} onClick={leftFunction}/>
         </div>
        
         <div id={"rimg"+Rstate}>
-            <p id="nameR">{game[round*2+1].name}</p>
-            <img id="img" src={game[round*2+1].src} onClick={()=>{
-                if(!isDisabled){
-                    setRState(1);
-                    setIsDisabled(true);
-                    setTimeout(()=>{
-                        setNextGame((prev)=>prev.concat(game[round*2+1])) 
-                        setRound(round=>round+1);
-                        setRState(0);
-                        setIsDisabled(false);
-                    },1000);
-                }
-                
-               
-            }}/>
+            <p id="nameR">{game[right].name}</p>
+            <img id="img" src={game[right].src} onClick={rightFunction}/>
         </div>
     </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Worldcup from './Worldcup.jsx'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
